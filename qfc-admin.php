c6b16276adecccc0e02525853bf24ce36f965827
John Clause <john.clause@gmail.com>
2016-06-21 16:06:57 -0700

    cleanup

--- qfc-admin.php
+++ qfc-admin.php
@@ -1,14 +1,12 @@
 <?php
 if(!defined('ABSPATH'))exit;
 
-//add_filter('qtranslate_load_admin_page_config','qfc_add_admin_page_config');//obsolete
 add_filter('i18n_admin_config','qfc_add_admin_page_config');
 function qfc_add_admin_page_config($page_configs)
 {
 	{
 	$page_config = array();
 	$page_config['pages'] = array( 'post.php' => '', 'post-new.php' => '' );
-	//"anchors":{"post-body-content":{"where":"first"},"postdivrich":{"where":"after"}},
 
 	$page_config['js-exec'] = array();
 	$js = &$page_config['js-exec']; // shorthand
